dcfab6f4a0e91af2abe6608d65e7f6028ffb116f
Matthew Phillips <matt@mattp.name>
2022-05-31 16:52:10 +0930

    Remove redundant ID's from test data

--- test/oo_events/tabular_report_test.clj
+++ test/oo_events/tabular_report_test.clj
@@ -4,17 +4,17 @@
             [oo-events.tabular-report :as tabular-report]))
 
 (deftest test-events-csv
-  (let [events  [{:id "1" :date #inst "2022-01-01T00:01" :task "task-1" :user "user-1" :status :complete}
-                 {:id "2" :date #inst "2022-01-01T00:02" :task "task-1" :user "user-1" :status :overdue}
-                 {:id "3" :date #inst "2022-01-01T00:03" :task "task-2" :user "user-1" :status :complete}
-                 {:id "4" :date #inst "2022-01-01T00:04" :task "task-1" :user "user-2" :status :complete}
-                 {:id "4" :date #inst "2022-01-01T00:05" :task "task-1" :user "user-2" :status :late}
+  (let [events  [{:date #inst "2022-01-01T00:01" :task "task-1" :user "user-1" :status :complete}
+                 {:date #inst "2022-01-01T00:02" :task "task-1" :user "user-1" :status :overdue}
+                 {:date #inst "2022-01-01T00:03" :task "task-2" :user "user-1" :status :complete}
+                 {:date #inst "2022-01-01T00:04" :task "task-1" :user "user-2" :status :complete}
+                 {:date #inst "2022-01-01T00:05" :task "task-1" :user "user-2" :status :late}
 
-                 {:id "5" :date #inst "2022-01-02T00:01" :task "task-1" :user "user-1" :status :overdue}
-                 {:id "6" :date #inst "2022-01-02T00:02" :task "task-1" :user "user-1" :status :late}
-                 {:id "7" :date #inst "2022-01-02T00:03" :task "task-2" :user "user-2" :status :complete}
-                 {:id "8" :date #inst "2022-01-02T00:04" :task "task-2" :user "user-2" :status :overdue}
-                 {:id "9" :date #inst "2022-01-02T00:05" :task "task-1" :user "user-2" :status :late}]
+                 {:date #inst "2022-01-02T00:01" :task "task-1" :user "user-1" :status :overdue}
+                 {:date #inst "2022-01-02T00:02" :task "task-1" :user "user-1" :status :late}
+                 {:date #inst "2022-01-02T00:03" :task "task-2" :user "user-2" :status :complete}
+                 {:date #inst "2022-01-02T00:04" :task "task-2" :user "user-2" :status :overdue}
+                 {:date #inst "2022-01-02T00:05" :task "task-1" :user "user-2" :status :late}]
         csv-rows ["Day,Task,User,Complete,Late,Overdue"
                   "2022-01-01T00:00:00Z,task-1,user-1,1,0,1"
                   "2022-01-01T00:00:00Z,task-1,user-2,1,1,0"
